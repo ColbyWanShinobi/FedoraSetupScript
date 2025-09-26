93752c7470ad65432e3bc63bb10bfba1b383894c
Colby Butler <colby@owljet.com>
2025-09-26 00:45:47 -0400

    Nobara installs

---
+++ install-ffmpeg.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
---
+++ update-xwayland.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+PACKAGE_NAME="xorg-x11-server-Xwayland"
+
+NOBARA_URL="https://download.copr.fedorainfracloud.org/results/gloriouseggroll/nobara-42/fedora-42-x86_64/"
+
+# Get the Fedora version
+FEDORA_VERSION=$(rpm -E %fedora)
+
+# get the version of the current xwayland package
+CURRENT_PACKAGE_VERSION=$(rpm -q --qf '%{VERSION}-%{RELEASE}' $PACKAGE_NAME)
+
+# Get the page content and search it for a link to the Xwayland package
+NOBARA_PACKAGE_DIR=$(curl -s "$NOBARA_URL" | grep -oP "href='[0-9]+-$PACKAGE_NAME'" | grep -oP "[0-9]+-$PACKAGE_NAME" | head -n 1)
+NOBARA_PACKAGE_URL="${NOBARA_URL}${NOBARA_PACKAGE_DIR}/$PACKAGE_NAME-$CURRENT_PACKAGE_VERSION.x86_64.rpm"
+
+echo "Current Xwayland version: $CURRENT_PACKAGE_VERSION"
+echo "Nobara Xwayland package URL: $NOBARA_PACKAGE_URL"
+
+# Download the Xwayland package
+#curl -LO "$XWAYLAND_PACKAGE_URL"
+# Download the file
+echo "Downloading file ${NOBARA_PACKAGE_URL} to ${HOME}/Downloads"
+curl --location --silent --fail --show-error --output ${HOME}/Downloads/$PACKAGE_NAME-$CURRENT_PACKAGE_VERSION.x86_64.rpm ${NOBARA_PACKAGE_URL}
+
+# Force install the downloaded package by name to avoid dependency issues
+sudo dnf reinstall --assumeyes ${HOME}/Downloads/$PACKAGE_NAME-$CURRENT_PACKAGE_VERSION.x86_64.rpm
